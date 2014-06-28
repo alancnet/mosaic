f2d6a1b55ece5f051ae07dcfdcb10645e4e93e84
alancnet <github@alanc.net>
2014-06-28 04:47:04 -0400

    Express.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-var http = require('http');
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('Hello World');
-}).listen(port);
+var express = require('express'),
+    app = express();
+
+
+app.get('/', function(req, res) {
+    res.send('Hello World :)');
+})
