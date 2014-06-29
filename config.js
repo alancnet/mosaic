034be40b8c74c835a0ede13ef5d5f98fdc4b006b
OpenShift guest <github@alanc.net>
2014-06-29 02:19:44 -0400

    Progress

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    twitterApiKey: 'ff1BYxIpMXtgF82m6DHGDuS5x',
+    twitterSecretKey: '2o8bNgclD44qqQykGUD19ShyQhVvbgcGSXuUDqu9cKDuAxqu3z',
+    sessionSecretKey: '1234567890QWERTY'
+}
--- server.js
+++ server.js
@@ -1,12 +1,56 @@
 var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 1337;
+    expressSession = require('express-session'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    passport = require('passport'),
+    TwitterStrategy = require('passport-twitter').Strategy,
+    config = require('./config'),
+    app = express();
 
+// App configure
+{
+    app.use(express.static('./www'));
+    app.use(expressSession({
+        secret: config.sessionSecretKey,
+        resave: false,
+        saveUninitialized: false
+    }));
+    app.use(cookieParser());
+    app.use(bodyParser());
+    app.use(passport.initialize());
+    app.use(passport.session());
+}
 
+// Twitter 
+{
+    app.get('/auth/twitter', passport.authenticate('twitter'));
+    
+    app.get('/auth/twitter/callback',
+        passport.authenticate('twitter', {
+            successRedirect: '/',
+            failureRedirect: '/login'
+        })
+    );
+    
+    passport.use(new TwitterStrategy({
+        consumerKey: config.twitterApiKey,
+        consumerSecret: config.twitterSecretKey,
+        callbackURL: "https://mosaic-c9-alancnet.c9.io/auth/twitter/callback"
+      },
+      function(token, tokenSecret, profile, done) {
+        console.log("Successful login: %o %o %o", token, tokenSecret, profile);
+        done(null, {findme: 'userObject'});
+      }
+    ));
+}
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}));
 app.get('/test', function(req, res) {
     res.send('Hello World :)');
 })
 
-app.use(express.static('./www'));
+app.listen(process.env.PORT, process.env.IP);
 
-app.listen(port);
+
