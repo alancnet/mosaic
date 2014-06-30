739187ed0b0c43aa2e181561405a2269ecd5b28c
Alan Colon <github@alanc.net>
2014-06-30 10:24:35 -0700

    ...

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     twitterApiKey: 'ff1BYxIpMXtgF82m6DHGDuS5x',
     twitterSecretKey: '2o8bNgclD44qqQykGUD19ShyQhVvbgcGSXuUDqu9cKDuAxqu3z',
-    sessionSecretKey: '1234567890QWERTY'
+    sessionSecretKey: '1234567890QWERTY',
+    cookieSecretKey: 'abcdefg1234567890'
 }
